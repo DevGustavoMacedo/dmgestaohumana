b304f1cd7e5b905e436f310657e2a94acd4b3dc1
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-12-09 19:57:11 -0300

    feat: home mobile done

---
+++ components/TypeItTitle.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import TypeIt from "typeit-react"
+
+export default function TypeItTitle() {
+  return (
+    <h1
+      className="font-title text-brand-yellow text-2xl bg-brand-darkGray w-fit px-4 py-2 bg-opacity-50 backdrop-blur-md rounded-md text-center 
+    md:text-7xl"
+    >
+      <TypeIt
+        options={{
+          speed: 150,
+          waitUntilVisible: true,
+        }}
+        getBeforeInit={(instance) => {
+          instance.type('Mais que RH.').pause(600).delete(12).type('Somos Gestão Humana.')
+
+          return instance
+        }}
+      />
+    </h1>
+  )
+}
